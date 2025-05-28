4d58f20703fd8fbc468072dd93610cffa1d48143
Evelina4355 <tinasejn@gmail.com>
2025-05-28 11:05:38 +0300

    Add files via upload

---
+++ task9.py
@@ -0,0 +1,19 @@
+print(f'Пример 1')  # выводим номер задания с помощью f-строки
+for x in 0,1:
+    for y  in 0,1:
+        for z in 0,1:
+            for w in 0,1:
+                F = (x or y) and not (y == z) and not (w)
+                if F = = 1: # пишут просто if F :
+                    print(x, y, z, w, F)
+
+print(f'Пример 2')  # выводим  ысю таблицу
+for x in 0, 1 :
+    for y in 0, 1 :
+        for z in 0, 1 :
+            for w in 0, 1 :
+                F = (x or y) and not (y ==z ) and not (w)
+
+                print(x, y, w, F)
+
+
