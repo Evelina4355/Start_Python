6e836346324ce0e318c0c4ac88273d6027565577
Evelina4355 <tinasejn@gmail.com>
2025-06-09 22:47:28 +0300

    task6.py

--- task6.py
+++ task6.py
@@ -1,1 +1,43 @@
-
+a.create #  СОЗДАТЬ
+a.retrive #
+a.update #
+a.delete #
+a = {1, 1.2, 'hello' }
+# CREATE СОЗДАТЬ
+a = {} #  пустое множество
+b = set() #
+c = frozenset() #
+a = { 1, 1.1, 'a' (1, 1.1), None True print} #
+a = { 1, 1.1, [1,2]} #
+a = frozenset({1, 1.1}) #
+a = [1, 'abc', 1] #
+sey(a) #
+b = set
+c = frozenset('hello') #
+d = set{[1, [2.1], 1}
+# UPDATE ИЗМЕНИТЬ
+# DELETE УДАЛИТЬ
+a = {1, 1.1, 'a'}
+a.add('') #
+b = 1
+a.add(b)
+a = {1,2,3}
+a.update
+#
+a = {1, 1.1, 'a'}
+a = {1, 1.1, 'a'}
+a = {1, 1.1, 'a'}
+#
+a = {1,2,3}
+b = (3,5,6)
+c = a - b
+print(c)
+a.copy() #
+a.isdisjoint({4,5,6}) #
+help(set) #
+a = {1,2,3}
+a.copy #
+a = {'сыр','помидор','молоко'}
+b = { 'яблоко','хлеб','молоко'}
+a.uion(b) # Возращает множество в котором элементы соединены в одномножество 
+a.union_update(b) # Записывает в множество "a" результатметода union
