bb5a848fe0185da1575f9c9cc5d5782d657f3131
Evelina4355 <tinasejn@gmail.com>
2025-06-10 10:07:57 +0300

    task9.py

--- task9.py
+++ task9.py
@@ -1,19 +1,55 @@
-print(f'Пример 1')  # выводим номер задания с помощью f-строки
-for x in 0,1:
-    for y  in 0,1:
-        for z in 0,1:
-            for w in 0,1:
-                F = (x or y) and not (y == z) and not (w)
-                if F = = 1: # пишут просто if F :
-                    print(x, y, z, w, F)
-
-print(f'Пример 2')  # выводим  ысю таблицу
-for x in 0, 1 :
-    for y in 0, 1 :
-        for z in 0, 1 :
-            for w in 0, 1 :
-                F = (x or y) and not (y ==z ) and not (w)
-
-                print(x, y, w, F)
-
-
+# print(f'Пример 1')  # выводим номер задания с помощью f-строки
+# for x in 0,1:
+#    for y  in 0,1:
+#        for z in 0,1:
+#           for w in 0,1:
+#               F = (x or y) and not (y == z) and not (w)
+#                if F = = 1: # пишут просто if F :
+#                    print(x, y, z, w, F)
+#
+# print(f'Пример 2')  # выводим  ысю таблицу
+# for x in 0, 1 :
+#    for y in 0, 1 :
+#       for z in 0, 1 :
+#           for w in 0, 1 :
+#                F = (x or y) and not (y ==z ) and not (w)
+#
+#                print(x, y, w, F)
+# 3) Запишем нашу функцию на языке программирования:
+# F = (x or y) and not(y == z) and not(w)
+# 4) чтобы выводить не полную таблицу истинности, а только те троки, в которых функция равна 1, добавим условие вывода:
+# if F:
+#     print(x, y, z,w)
+# после запуска программы получаем все интересубщие нас строки.
+# print(f'Пример 1')  # выводим номер задания с помощью f-строки
+# for x in 0,1:
+#    for y  in 0,1:
+#        for z in 0,1:
+#           for w in 0,1:
+#               F = (x or y) and not (y == z) and not (w)
+#                if F = = 1: # пишут просто if F :
+#                    print(x, y, z, w, F)
+# for a in 0,1:
+#   for b in 0, 1:
+#      for c in 0, 1:
+#           F = not(a or b) and c
+#              # if F:
+#              print(a, b, c, F)
+# for x in 0,1:
+#   for y in 0, 1:
+#      for z in 0, 1:
+#           F = not(x or y) <= z
+#              # if F:
+#                print(x, y, z, F)
+# for s1 in 0,1:
+#   for s2 in 0, 1:
+#      for s3 in 0, 1:
+#           F = s1 and not s2 or s3
+#              # if F:
+#                print(s1, s2, s3, F)
+# for x in 0,1:
+#   for y in 0, 1:
+#      for z in 0, 1:
+#           F = not x == y or z
+#              # if F:
+#                print(x, y, z, F)
